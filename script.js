f526397a0a91e03e422a953d9d656a2d75871f98
Harrison <harrisondsouza76@gmail.com>
2023-02-16 23:08:12 +0530

    Add reading status functionality to book card and object

--- script.js
+++ script.js
@@ -33,12 +33,19 @@ function addBook(){
   const bookName = document.querySelector("#main .modalBg .addBookForm .bookNameInp").value;
   const authorName = document.querySelector("#main .modalBg .addBookForm .authorNameInp").value;
   const pages = document.querySelector("#main .modalBg .addBookForm .pagesInp").value;
+  const radioValue = document.getElementsByName("readingStatus");
+  let readingStatus = undefined;
+  for(let i = 0; i< radioValue.length; i++){
+    if(radioValue[i].checked){
+       readingStatus = radioValue[i].value;
+      }
+   }
 
-  const newBook = new Book(bookName, authorName, pages);
+
+  const newBook = new Book(bookName, authorName, pages, readingStatus);
   library.push(newBook);
   refreshLibraryContent();
 }
-
 
 
 
@@ -50,10 +57,11 @@ function refreshLibraryContent(){
 }
 
 
-function Book(name, authorName, pages){
+function Book(name, authorName, pages, readingStatus){
   this.name = name;
   this.authorName = authorName;
   this.pages = pages;
+  this.readingStatus = readingStatus;
 
 
 
@@ -61,24 +69,66 @@ function Book(name, authorName, pages){
 
     const card = document.createElement('div');
     card.classList = 'book';
+
+    // read radio
+    this.read = document.createElement('input');
+    this.read.setAttribute('name', `${library.indexOf(this)}readingStatus`)
+    this.read.setAttribute('type', 'radio');
+    this.read.id = `${library.indexOf(this)}read`;
     
+    // not read radio
+    this.notRead = document.createElement('input');
+    this.notRead.setAttribute('name', `${library.indexOf(this)}readingStatus`)
+    this.notRead.setAttribute('type', 'radio');
+    this.notRead.id  = `${library.indexOf(this)}not-read`;
+    
+    // remove button
     this.removeBtn = document.createElement('button');
     this.removeBtn.classList = "btn-rem";
     this.removeBtn.textContent = "Remove";
+
+
     const cardContent = `
         <p class="bookName">${name}</p>
         <p class="author">~By <span>${authorName}</span></p>
         <p class="pages"><span>${pages}</span> pages</p>
         <div class="status">
-          <input type="radio" id="read" name="readingStatus" value="read />
-          <label for="read">Read</label>
-          <input type="radio" id="not-read" name="readingStatus" value="not-read />
-          <label for="not-read>Not Read</label>
+        <div class="read-group">
+         
+          <label for="${library.indexOf(this)}read">Read</label>
+        </div>
+        <div class="notRead-group">
+          
+          <label for="${library.indexOf(this)}not-read">Not Read</label>
+        </div>
         </div>
     `;
   
     
+    
     card.innerHTML = cardContent;
+
+
+    // implement read status in card and book object
+    card.querySelector(".read-group").insertBefore(this.read, card.querySelector(".read-group label"));
+    card.querySelector(".notRead-group").insertBefore(this.notRead, card.querySelector(".notRead-group label"));
+
+
+    this.read.addEventListener("click", ()=>{
+      this.readingStatus = "read"
+    })
+    this.notRead.addEventListener("click", ()=>{
+      this.readingStatus = "not-read"
+    })
+
+    if(this.readingStatus == "read"){
+      this.read.checked = true;
+    } else {
+      this.notRead.checked = true;
+    }
+
+
+    // implement remove button for book card
     card.appendChild(this.removeBtn);
     bookList.appendChild(card);
     
@@ -89,35 +139,4 @@ function Book(name, authorName, pages){
   }
 
 }
-// book layout
-
-  //  book card
-
-  
-
-
-
-
-
-// function createBookCard(title, authorName, pages, removeBtn){
-//   const card = document.createElement('div');
-//   card.classList = 'book';
-
-//   const cardContent = `
-//       <p class="bookName">${title}</p>
-//       <p class="author">~By <span>${authorName}</span></p>
-//       <p class="pages"><span>${pages}</span> pages</p>
-//       <div class="status">
-//         <input type="radio" id="read" name="readingStatus" value="read />
-//         <label for="read">Read</label>
-//         <input type="radio" id="not-read" name="readingStatus" value="not-read />
-//         <label for="not-read>Not Read</label>
-//       </div>
-//   `;
-
-//   card.appendChild(removeBtn);
-
-//   card.innerHTML = cardContent;
-//   bookList.appendChild(card);
-// }
 
